e366a7efdf2c999b1a3cafa7a08f23781ff1c016
Alexey Isavnin <alexey.isavnin@helsinki.fi>
2011-10-14 18:28:29 +0300

    more robust lasco preparation script

--- prep_lasco.sh
+++ prep_lasco.sh
@@ -2,18 +2,19 @@
 
 zip_path=$1
 telescope=$2
-html_path=$zip_path".html"
 data_path=$(dirname $zip_path)
 zip_file=$(basename $zip_path)
 zip_name=$(basename $zip_file .zip)
-unzip -u $zip_path -d $data_path
-gunzip $data_path/$zip_name/*.fts.gz
+html_path=$data_path/$zip_name.html
+
+unzip -j -u $zip_path -d $data_path/$telescope
+gunzip $data_path/$telescope/*.fts.gz
 
 patterns=(`cat $html_path | sed -r 's/.*([0-9]{8})\.fts\s[0-9]+.*([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}).*/\1 \2_\3/g' | sed -r 's/^.*[a-zA-Z].*$//g'`)
 
 for i in $(seq 0 2 ${#patterns[@]})
 do
   filename=$(echo ${patterns[$i + 1]} | sed 's/-//g' | sed 's/://g')_"$telescope"L.fts
-  mv $data_path/$zip_name/${patterns[$i]}.fts $data_path/$filename
+  mv $data_path/$telescope/${patterns[$i]}.fts $data_path/$filename
 done
 
